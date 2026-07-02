e80586c4e9565018d23f02ab76618a60541d0e3b
keillergamoreira-maker <keillergamoreira@gmail.com>
2026-07-02 14:25:07 -0300

    Add files via upload

--- carregar_arquivo.py
+++ carregar_arquivo.py
@@ -5,7 +5,6 @@ import pandas as pd
 import streamlit as st
 
 
-#@st.cache_data(show_spinner='Abrindo arquivo...')
 def carregar_arquivo(upload):
     """Função responsável por carregar o arquivo de upload e mostrar no site a tabela."""
     #---Verificar qual tipo de arquivo foi verificado
